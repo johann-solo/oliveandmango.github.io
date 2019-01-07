6e2f86fa4ef7c2e3d3be5c80817ff058202eb878
Johann <johannd@icloud.com>
2019-01-06 23:15:26 -0500

    Search

--- src/js/app.js
+++ src/js/app.js
@@ -16,6 +16,7 @@ userFeed.run();
 
 require("./bloglovin.js");
 require("./lazyload.js");
+require("./search.js");
 
 // var client = algoliasearch("HRBO4CU3NQ", "72dc5eb31124694164a13ea54d64d115");
 // var index = client.initIndex("olive_mango");
