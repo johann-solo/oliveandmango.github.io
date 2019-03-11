0cf15efe1da7bcf092ccba92df5a5bf2fd60e020
Manfred Headley <manfred.headley@gmail.com>
2019-03-11 16:03:28 -0400

    fixing search

--- src/js/search.js
+++ src/js/search.js
@@ -1,6 +1,8 @@
 import Fuse from 'fuse.js'
 const list = require('../../dist/index.json')
 
+console.log('search')
+
 const options = {
   keys: ['title', 'tags'],
   minMatchCharLength: 3,
@@ -13,8 +15,9 @@ const button = document.getElementById('submitsearch')
 
 button.onclick = function () {
   const search = searchquery.value
+  localStorage.setItem('query', search)
   if (search) {
-    submitSearch(search)
+    window.location.href = `/search/?query=${search}` 
   }
 }
 
@@ -26,4 +29,10 @@ function submitSearch(query) {
     resultsContainer.appendChild(li)
     li.innerHTML = `<a href="${result.permalink}">${result.title}</a>`
   })
+}
+
+if(window.location.href.includes('search')) {
+  const urlParams = new URLSearchParams(window.location.search)
+  const myParam = urlParams.get('query')
+  submitSearch(myParam)
 }
