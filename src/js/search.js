6965d1def89b4dabb1b9386e33920f16b5ac6e6a
Manfred Headley <manfredheadley@pop-os.localdomain>
2021-01-14 17:13:42 -0500

    2 - adding search

--- src/js/search.js
+++ src/js/search.js
@@ -1,7 +1,4 @@
 import Fuse from 'fuse.js'
-const list = require('../../dist/index.json')
-
-console.log('search')
 
 const options = {
   keys: ['title', 'tags'],
@@ -9,30 +6,37 @@ const options = {
   threshold: 0.3
 }
 
-const fuse = new Fuse(list, options)
-
-const button = document.getElementById('submitsearch')
-
-button.onclick = function () {
-  const search = searchquery.value
-  localStorage.setItem('query', search)
-  if (search) {
-    window.location.href = `/search/?query=${search}` 
+const submitSearch = async (query) => {
+  try {
+    const response = await fetch('/index.json')
+    const list = await response.json()
+    const fuse = new Fuse(list, options)
+    const results = fuse.search(query)
+    const resultsContainer = document.getElementById('results')
+    document.getElementById('search_title').innerHTML = query
+    if(results.length > 0) {
+      results.forEach(result => {
+        const li = document.createElement('li')
+        resultsContainer.appendChild(li)
+        li.innerHTML = `
+          <a href="${result.item.permalink}">
+            <img src="${result.item.thumbnail}" alt="${result.item.title}">
+          </a>
+          <a href="${result.item.permalink}">${result.item.title}</a>
+        `
+      })
+    } else {
+      document.getElementById('no-results').innerHTML = "Oops! It doesn't look like we have any of those! Why not try another search."
+    }
+  } catch (error) {
+    console.log(error)
   }
-}
-
-function submitSearch(query) {
-  const results = fuse.search(query)
-  const resultsContainer = document.getElementById('results')
-  results.forEach(result => {
-    const li = document.createElement('li')
-    resultsContainer.appendChild(li)
-    li.innerHTML = `<a href="${result.permalink}"><img src="${result.thumbnail}" alt="${result.title}"></a><a href="${result.permalink}">${result.title}</a>`
-  })
 }
 
 if(window.location.href.includes('search')) {
   const urlParams = new URLSearchParams(window.location.search)
   const myParam = urlParams.get('query')
-  submitSearch(myParam)
+  if (myParam) {
+    submitSearch(myParam)
+  }
 }
